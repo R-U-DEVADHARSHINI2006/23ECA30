613e629de99d05c52a9316b8122612e424b381ca
Devadharshini R U <155797442+R-U-DEVADHARSHINI2006@users.noreply.github.com>
2026-06-30 12:35:56 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -112,6 +108,6 @@ function App() {
       <section id="spacer"></section>
     </>
   )
-}
+
 
 export default App
